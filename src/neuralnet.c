2cb0442f78aa203727c41df941234902d378d437
Erik Stolpe <erik.stolpe98@gmail.com>
2025-05-18 19:11:55 +0200

    Add skeleton neuralnet (wip)

--- src/neuralnet.c
+++ src/neuralnet.c
@@ -1,1 +1,29 @@
 #include "neuralnet.h"
+
+NeuralNet *neuralnet_create(int layer_input_size, int layer_output_size, int num_layers, int *layer_sizes)
+{
+}
+
+void neuralnet_set_activation(NeuralNet *nn, float (*activation)(float))
+{
+    nn->activation = activation;
+}
+
+void neuralnet_set_activation_derivative(NeuralNet *nn, float (*activation_derivative)(float))
+{
+    nn->activation_derivative = activation_derivative;
+}
+
+float ReLU(float x)
+{
+    return x > 0.0f ? x : 0.0f;
+}
+
+float ReLU_derivative(float x)
+{
+    return x > 0.0f ? 1.0f : 0.0f;
+}
+
+void neuralnet_load_model(NeuralNet *nn, const char *model_fp)
+{
+}
--- src/neuralnet.h
+++ src/neuralnet.h
@@ -1,4 +1,42 @@
 #ifndef NEURALNET_H
 #define NEURALNET_H
 
+#include "../linalg/include/linalg.h"
+#include "mnist.h"
+
+typedef struct
+{
+    int layer_input_size;
+    int layer_output_size;
+
+    int *layer_sizes;
+    int num_layers;
+
+    float (*activation)(float);
+    float (*activation_derivative)(float);
+
+    // Each layer has a weight matrix, e.g. hidden layer 0 node 0 has weights: weights[0][0] = [1, 2, ..., 1] to input layer nodes
+    Matrix **weights;
+
+    // Bias for each layer (excluding input layer)
+    Vector **biases;
+
+} NeuralNet;
+
+NeuralNet *neuralnet_create(int layer_input_size, int layer_output_size, int num_hidden_layers, int *layer_sizes);
+
+void neuralnet_load_model(NeuralNet *nn, const char *model_fp);
+void neuralnet_set_activation(NeuralNet *nn, float (*activation)(float));
+void neuralnet_set_activation_derivative(NeuralNet *nn, float (*activation_derivative)(float));
+
+float ReLU(float x);
+float ReLU_derivative(float x);
+
+void neuralnet_init_w_b(NeuralNet *nn);
+
+void neuralnet_train(NeuralNet *nn, const Mnist *mnist);
+void neuralnet_test(const NeuralNet *nn, const Mnist *mnist);
+
+int neuralnet_infer(const NeuralNet *nn, const Vector *image);
+
 #endif
